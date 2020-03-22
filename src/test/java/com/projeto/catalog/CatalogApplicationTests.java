b7092c13ab25f6489b11483726c51f14f1dbaea1
Joao Fulgencio <joao.fulgencio@zup.com.br>
2020-03-22 16:22:06 -0300

    Removing wrong test test

--- src/test/java/com/projeto/catalog/CatalogApplicationTests.java
+++ src/test/java/com/projeto/catalog/CatalogApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootApplication
 class CatalogApplicationTests {
 
 	@Test
